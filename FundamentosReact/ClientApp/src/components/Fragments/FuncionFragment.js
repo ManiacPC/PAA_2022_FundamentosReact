01ba8ac704c761f3d37ed1ced650727aa26e587b
ENRIQUE ANDRES PALACIOS CARRASCO <enrique.palacios@inacapmail.cl>
2022-07-05 12:54:16 -0400

    Subcomponentes con hijos, corrección de enlaces a botones

---
+++ FundamentosReact/ClientApp/src/components/Fragments/FuncionFragment.js
@@ -0,0 +1,23 @@
+﻿import React, { useState, useEffect } from 'react'
+
+const FuncionFragment = ({ titulo1, titulo2, handleBoton, children }) => {
+
+    return (
+        <>
+            <div className="card">
+                <div className="card-header">
+                    {titulo1}
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">{titulo2}</h5>
+                    <p className="card-text">
+                        {children}
+                    </p>
+                    <button className="btn btn-primary" onClick={handleBoton}>Saludar</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FuncionFragment
--- FundamentosReact/ClientApp/src/components/Funcion.js
+++ FundamentosReact/ClientApp/src/components/Funcion.js
@@ -1,9 +1,23 @@
 ﻿import React, { useState, useEffect } from 'react'
+import FuncionFragment from './Fragments/FuncionFragment'
 
 const Funcion = () => {
+    const [nombre, setNombre] = useState("")
 
     const handleSaludo = (evento) => {
-        alert("Hola que tal?")
+        alert("¿Hola que tal?")
+    }
+
+    const handleSaludoNombre = (evento) => {
+        if (nombre === '') {
+            alert('Por favor escriba un nombre para poder saludarlo')
+        } else {
+           alert(`Hola ${nombre}!`)
+        }
+    }
+
+    const handleNombre = (evento) => {
+        setNombre(evento.target.value)
     }
 
     return (
@@ -12,37 +26,39 @@ const Funcion = () => {
 
             <div className="row">
                 <div className="col-12">
-                    <div className="card">
-                        <div className="card-header">
-                            Función 1
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title">Función saludar</h5>
-                            <p className="card-text">
-                                Aquí se presenta una breve descripción de lo que queremos
-                                utilizar en React.
-                            </p>
-                            <a href="#" className="btn btn-primary" onClick={handleSaludo}>Saludar</a>
-                        </div>
-                    </div>
+                    <FuncionFragment
+                        titulo1="Función 1"
+                        titulo2="Función saludar"
+                        handleBoton={handleSaludo}
+                    >
+                        Aquí se presenta una breve descripción de lo que queremos
+                        utilizar en React.
+                    </FuncionFragment>
                 </div>
             </div>
 
             <div className="row mt-4">
                 <div className="col-12">
-                    <div className="card">
-                        <div className="card-header">
-                            Función 1
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title">Función saludar</h5>
-                            <p className="card-text">
-                                Aquí se presenta una breve descripción de lo que queremos
-                                utilizar en React.
-                            </p>
-                            <a href="#" className="btn btn-primary" onClick={handleSaludo}>Saludar</a>
-                        </div>
-                    </div>
+                    <FuncionFragment
+                        titulo1="Función 2"
+                        titulo2={`Función saludar con nombre`}
+                        handleBoton={handleSaludoNombre}
+                    >
+                        <p>
+                            Por favor escriba su nombre y salude en caso de que efectivamente
+                            haya escrito uno
+                        </p>
+                        <label htmlFor="nombre">Nombre</label>
+                        <input type="text"
+                            className="form-control mb-2"
+                            value={nombre}
+                            onChange={handleNombre}
+                        />
+                        {nombre === '' &&
+                            <p className="text-sm text-danger">Por favor escriba un nombre</p>
+                        }
+
+                    </FuncionFragment>
                 </div>
             </div>
         </>
